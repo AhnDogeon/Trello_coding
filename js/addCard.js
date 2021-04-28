550d4f56e6c032bd42ba6b51486b9936a3b3f2a9
AhnDogeon <qltiqlti@gmail.com>
2021-04-28 23:01:50 +0900

    210428 | trello

---
+++ js/addCard.js
@@ -0,0 +1,23 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greeting");
+
+const CARD_LS = "currentCard";
+
+function loadCard() {
+    const currentCard = localStorage.getItem(CARD_LS);
+    if (currentCard === null){
+
+
+    } else{
+
+    }
+}
+
+
+
+function init(){
+
+}
+
+init();
